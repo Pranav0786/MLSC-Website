aa1dde28ea680c62d24dcfaf266fe359e50c9c35
Pranav Surve <pranavsurve.0786@gmail.com>
2024-09-16 14:19:05 +0530

    started website

---
+++ mlsc/src/Data/MainBoard.jsx
@@ -0,0 +1,99 @@
+export const MainBoard = [
+    {
+        name: "Akshat Mahajan",
+        position: "Lead, Cloud Lead",
+        image: ""
+    },
+    {
+        name: "Abhishek Dudhpachare",
+        position: "Co-Lead, Cybersecurity Lead",
+        image: ""
+    },
+    {
+        name: "Rucha Ikare",
+        position: "Secretary, Treasurer",
+        image: ""
+    },
+    {
+        name: "Aditya Tilak",
+        position: "Event Coordinator",
+        image: ""
+    },
+    {
+        name: "Viraj Gotmare",
+        position: "Event Coordinator",
+        image: ""
+    },
+    {
+        name: "Pranav Surve",
+        position: "Technical Lead",
+        image: ""
+    },
+    {
+        name: "Hemant Sharma",
+        position: "Technical Lead",
+        image: ""
+    },
+    {
+        name: "Jayshree Mali",
+        position: "Marketing and Social Engagement Lead",
+        image: ""
+    },
+    {
+        name: "Aishawarya Rajput",
+        position: "Marketing and Social Engagement Lead",
+        image: ""
+    },
+    {
+        name: "Gaytri Dabhade",
+        position: "Design and Content Head",
+        image: ""
+    },
+    {
+        name: "Shruti Chavan",
+        position: "Design and Content Head",
+        image: ""
+    },
+    {
+        name: "Athrav Pandey",
+        position: "Development Lead",
+        image: ""
+    },
+    {
+        name: "Hrushikesh Deshpande",
+        position: "AI ML Lead",
+        image: ""
+    },
+    {
+        name: "Akshay Kumar Kachroo",
+        position: "Blog Writer",
+        image: ""
+    },
+    {
+        name: "Priti Desai",
+        position: "Social Handle Manager",
+        image: ""
+    },
+    {
+        name: "Mihir Pusadkar",
+        position: "Editor",
+        image: ""
+    },
+
+    {
+        name: "Hamza Shaikh",
+        position: "Advisory Member",
+        image: ""
+    },
+    {
+        name: "Sharaneshwar Punjal",
+        position: "Advisory Member",
+        image: ""
+    },
+    {
+        name: "Om Kulkarni",
+        position: "Advisory Member",
+        image: ""
+    },
+    
+] ;
---
+++ mlsc/src/components/Sponsor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sponsor = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sponsor
