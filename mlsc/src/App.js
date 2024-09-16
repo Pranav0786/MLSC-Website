965b163e2077fa9dc1f0828e989154862bc824bf
Pranav Surve <pranavsurve.0786@gmail.com>
2024-09-16 14:31:46 +0530

    added all necessary things

--- mlsc/src/App.js
+++ mlsc/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes  } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
       <div>
-        Hello
+        <Routes>
+            <Route path="/" element={  <Home />} />
+
+        </Routes>
       </div>
   );
 }
